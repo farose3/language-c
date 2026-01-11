73f1f5396bce63f7ccac890c8d6659d1fca192b2
farose3 <faroslaksnaphirmy@gmail.com>
2026-01-11 16:07:46 +0700

    completed version of calculator

--- Challenge/Challenge2.c
+++ Challenge/Challenge2.c
@@ -1,44 +1,56 @@
 #include<stdio.h>
 
-int plusNumber(int a, int b) {
-    int sum = a+b ;
+float plusNumber(float a, float b) {
+    float sum = a+b ;
     return sum;
 }
 
-int minusNumber(int a, int b) {
-    int minus = a-b ;
+float minusNumber(float a, float b) {
+    float minus = a-b ;
     return minus;
 }
 
-int multiplyNumber(int a, int b) {
-    int _time = a*b ;
+float multiplyNumber(float a, float b) {
+    float _time = a*b ;
     return _time;
 }
 
-int divideNumber(int a, int b) {
-    int divide = a/b ;
+float divideNumber(float a, float b) {
+    float divide = a/b ;
     return divide;
 }
 
 
+// It's done
+
+
 int main() {
     
-    int a,b;
-    int result;
-    int c;
+    float a,b;
+    float result;
+    char c;
     
     printf("Please select any number : ");
-    scanf("%d", &a);
+    scanf("%f", &a);
     
     printf("Please selest an operator : ");
-    scanf("%d", &c);
+    scanf(" %c", &c);
     
     printf("Please select any number : ");
-    scanf("%d", &b);
+    scanf("%f", &b);
     
-    if (c == 3) {
+    if (c == '+'){
         result = plusNumber(a,b);
-        printf("result : %d",result);
+        printf("result : %f",result);
+    }else if (c == '/'){
+        result = divideNumber(a,b);
+        printf("result : %f",result);
+    }else if (c == '-'){
+        result = minusNumber(a,b);
+        printf("result : %f",result);
+    }else if (c == '*'){
+        result = multiplyNumber(a,b);
+        printf("result : %f",result);
     }else {
         printf("re-enter your operator");
     }
