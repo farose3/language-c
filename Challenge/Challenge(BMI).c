1000657fbb0f18717d5983030d5f5960e1aa1ec4
farose3 <faroslaksnaphirmy@gmail.com>
2026-02-15 16:08:55 +0700

    not done

---
+++ Challenge/Challenge(BMI).c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+float calculation1(float Mark's mass,float Mark's height) {
+    float a = Mark's mass/Mark's height**2 ;
+    return a ;
+}
+
+
+float calculation2(float John's mass,float John's height) {
+    float b = John's mass/John's height**2 ;
+    return b ;
+}
+
+int main() {
+    
+    float Mark's mass = 78
+    float Mark's height = 1.69
+    
+    float John's mass = 92
+    float John's height = 1.95
+    
+    // show the result
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
